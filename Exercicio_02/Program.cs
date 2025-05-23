4615541c092f0469d45a14257efa0d19ba29d47c
Matheus Gomes F. Eusebio <matt-xp2@live.com>
2025-05-23 11:33:17 -0300

    Começado o exercício 02 e feito o exercício 03

---
+++ Exercicio_02/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Exercicio_02;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        List<Dictionary<string, object>> list = new List<Dictionary<string, object>>();
+        int id = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Clear();
+            Console.WriteLine($"Cadastro do {i}º Produto");
+            Console.Write("Digite o nome do produto: ");
+            string nameProduct = Console.ReadLine();
+
+            Console.Write("Digite o preço do produto: ");
+            bool sucess = decimal.TryParse(Console.ReadLine(), out decimal priceProduct);
+            while (!sucess || priceProduct <= 0)
+            {
+                Console.WriteLine("Digite um preço válido!!!!!");
+                Console.Write("Digite o preço do produto: ");
+                sucess = decimal.TryParse(Console.ReadLine(), out priceProduct);
+            }
+
+            Console.Write("Digite a quantidade do produto: ");
+            sucess = int.TryParse(Console.ReadLine(), out int quantityProduct);
+            while (!sucess || quantityProduct <= 0)
+            {
+                Console.WriteLine("Quantitade inválida!!!");
+                Console.Write("Digite a quantidade do produto: ");
+                sucess = int.TryParse(Console.ReadLine(), out quantityProduct);
+            }
+
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            id++;
+            dict.Add("Id", id);
+            dict.Add("Nome", nameProduct);
+            dict.Add("Preço", priceProduct);
+            dict.Add("Quantidade", quantityProduct);
+
+            list.Add(dict);
+        }
+
+        bool exit = false;
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("==== MENU CONTROLE DE ESTOQUE ====");
+            Console.WriteLine("[1] - COMPRAR PRODUTO");
+            Console.WriteLine("[2] - REABASTECER ESTOQUE");
+            Console.WriteLine("[3] - SAIR");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("==== MENU COMPRA PRODUTO ====");
+                    foreach (var item in list)
+                    {
+                        foreach (var obj in item)
+                        {
+                            if (obj.Key.Equals("Id"))
+                            {
+                                Console.WriteLine($"Opção - [{obj.Value}]");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{obj.Key} = {obj.Value}");
+                            }
+                        }
+                    }
+                    Console.Write("Escolha uma das opções: ");
+                    string inputUser = Console.ReadLine();
+                    int valueIdList = IsValidInt(inputUser);
+                    if (list.Exists(x => x.Values.Equals(valueIdList)))
+                    {
+                        var productBuy = list.Find(x => x.Values.Equals(valueIdList));
+                        Console.Write("Informe a quantidade que deseja retirar: ");
+                        int quantityBuy = IsValidInt(Console.ReadLine());
+                        if ((int)productBuy["Quantidade"] <= quantityBuy)
+                        {
+                            Console.WriteLine("Compra realizada com sucesso!");
+                            Console.WriteLine($"{productBuy["Quantidade"]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Quantidade superior a capacidade de estoque");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não existe esse produto no nosso estoque!");
+                        await Task.Delay(2000);
+                    }
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Digite uma opção válida!!!!");
+                    await Task.Delay(2000);
+                    break;
+            }
+
+
+        }
+        
+        static int IsValidInt(string inputUser)
+        {
+            bool sucess = int.TryParse(inputUser, out int validNumber);
+            while (!sucess || validNumber <= 0)
+            {
+                Console.Write("Digite um número válido acima de zero: ");
+                inputUser = Console.ReadLine();
+                sucess = int.TryParse(inputUser, out validNumber);
+            }
+            return validNumber;
+        }
+    }
+}
---
+++ Exercicio_03/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Exercicio_03;
+
+class Program
+{
+    static List<Dictionary<string, DateTime>> list = new List<Dictionary<string, DateTime>>();
+    static async Task Main(string[] args)
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("==== MENU CADASTRO DE EVENTOS ====");
+            Console.WriteLine("[1] - CADASTRAR EVENTO");
+            Console.WriteLine("[2] - LISTAR EVENTOS");
+            Console.WriteLine("[3] - SAIR DO EVENTO");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("==== MENU CADASTRO EVENTO ====");
+                    Console.Write("Nome: ");
+                    string nameEvent = Console.ReadLine();
+
+                    Console.Write("Data e hora (dd/MM/yyyy) HH:mm ");
+                    DateTime dateEvent = IsValidDate(Console.ReadLine());
+
+                    Console.WriteLine("Evento cadastrado com sucesso!");
+
+                    Dictionary<string, DateTime> dict = new Dictionary<string, DateTime>();
+                    dict.Add(nameEvent, dateEvent);
+                    list.Add(dict);
+
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("==== MENU DE EVENTOS CADASTRADOS ====");
+                    int count = 1;
+                    foreach (var item in list)
+                    {
+                        foreach (var obj in item)
+                        {
+                            Console.WriteLine($"Evento {count}");
+                            count++;
+                            Console.WriteLine($"O evento: {obj.Key} acontecerá as {obj.Value}");
+                            Console.WriteLine();
+                        }
+                    }
+                    Console.WriteLine("Digite qualquer tecla para voltar ao Menu:");
+                    Console.ReadLine();
+                    break;
+                case "3":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Digite uma opção válida");
+                    await Task.Delay(2000);
+                    break;
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine("Obrigado por usar o programa!");
+    }
+
+    static DateTime IsValidDate(string inputUser)
+    {
+        bool sucess = DateTime.TryParseExact(inputUser, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateConvert);
+        bool existDateEvent = list.Any(x => x.Values.First() == dateConvert);
+        Console.WriteLine(existDateEvent);
+        while (!sucess || existDateEvent || dateConvert < DateTime.Now)                                                                                                                                                                                                                                                                                                        
+        {
+            Console.Write("A data digitada é inválida, por favor digite outra data: ");
+            inputUser = Console.ReadLine();
+            sucess = DateTime.TryParseExact(inputUser, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateConvert);
+            existDateEvent = list.Exists(x => x.Values.Equals(dateConvert));
+        }
+
+        return dateConvert;
+    }
+}
