db5e5bec48feb8bc15d601483002daddb57d0cf8
Matheus Gomes F. Eusebio <matt-xp2@live.com>
2025-05-23 10:33:01 -0300

    Foi realizado a 01 questão

---
+++ Exercicio_01/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercicio_01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputUser;
+
+        Console.Write("Nome: ");
+        inputUser = Console.ReadLine();
+        string name = IsValidName(inputUser);
+
+        Console.Write("CPF: ");
+        inputUser = Console.ReadLine();
+        string cpf = IsValidCPF(inputUser);
+
+        Console.Write("Idade: ");
+        inputUser = Console.ReadLine();
+        string age = IsValidAge(inputUser);
+
+        Console.Clear();
+        Console.WriteLine("Cadastro aprovado!");
+        Console.WriteLine($"Nome: {name}");
+        Console.WriteLine($"CPF: {cpf}");
+        Console.WriteLine($"Idade: {age}");
+
+
+    }
+
+    static string IsValidName(string inputUser)
+    {
+        while (inputUser.Length < 5)
+        {
+            Console.Write("Digite um nome maior que 05 caracteres: ");
+            inputUser = Console.ReadLine();
+        }
+
+        return inputUser;
+    }
+
+    static string IsValidCPF(string inputUser)
+    {
+        Regex regex = new Regex(@"^(\d{3})\.?(\d{3})\.?(\d{3})-?(\d{2})$");
+        while (inputUser.Length < 11)
+        {
+            Console.Write("Digite um CPF válido: ");
+            inputUser = Console.ReadLine();
+        }
+        string cpf = inputUser;
+        string cpfFormatado = regex.Replace(cpf, "$1.$2.$3-$4");
+
+        return cpfFormatado;
+    }
+
+    static string IsValidAge(string inputUser)
+    {
+        bool sucess = int.TryParse(inputUser, out int age);
+        while (!sucess || age < 18)
+        {
+            Console.Write("Digite uma data superior a 18: ");
+            inputUser = Console.ReadLine();
+        }
+        return $"{age} anos";
+    }
+    
+}
